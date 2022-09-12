6244bb724e5630a702ea9f2bb9e820392ef655b2
Marcio Bispo <91513221+EngMarcioBispo@users.noreply.github.com>
2022-09-11 23:10:59 -0300

    Versão 1.0

--- FrontEnd/src/app/components/cars/cars.service.ts
+++ FrontEnd/src/app/components/cars/cars.service.ts
@@ -13,7 +13,7 @@ import { MatSnackBar } from "@angular/material/snack-bar";
 })
 export class CarsService {
 
-  baseUrl = "http://localhost:3000/cars";
+  baseUrl = "https://api-cars-new.herokuapp.com/cars";
 
   constructor(private snackBar: MatSnackBar, private http: HttpClient) { }
 
